880f25d562220ed51caa399991d02f53601304e4
Brosa <brunoarosa1997@gmail.com>
2023-03-04 15:09:12 -0500

    created a function to handle current weather

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,29 @@
 var submitBtn = document.getElementById('submitbtn')
 
+function grabCurrentdata(name, obj, txt) {
+    var currentDate = obj.dt_txt.split('-')
+    var dateReorg = `${currentDate[1]}/${currentDate[2].slice(0, 2)}/${currentDate[0]}`
+    var icon = obj.weather[0].icon
+    var iconSrc = `http://openweathermap.org/img/wn/${icon}.png`
+    var currentTemp = `Temp: ${obj.main.temp}°F`
+    var currentWind = `Wind: ${obj.wind.speed}MPH`
+    var currentHumidity = `Humidity: ${obj.main.humidity}%`
+
+    console.log(name)
+    console.log(icon)
+    console.log(currentTemp)
+    console.log(currentWind)
+    console.log(currentHumidity)
+
+        // add to HTML main
+        var cityHeaderEl = document.querySelector('#cityheader')
+        cityHeaderEl.innerHTML = `${txt} (${dateReorg}) <img src=${iconSrc}>`
+
+        // add to HTML aside
+        //var prevCitiesEl = document.querySelector('#previouscities')
+        //prevCitiesEl.innerHTML += `<a href="#" class="list-group-item list-group-item-action active" aria-current="true">${inputCityEl.toUpperCase()}</a>`
+}
+
 function handleSubmittedCity(event) {
     event.preventDefault()
 
@@ -11,37 +35,21 @@ function handleSubmittedCity(event) {
     }
     // API URL
     var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${inputCityEl.toLowerCase()}&appid=9d4a069564580646340081bd6f2b3b20&units=imperial`
-    
+
     fetch(weatherURL)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
+            console.log(data)
             var cityName = data.city.name
             var currentData = data.list[0]
-            var currentDate = currentData.dt_txt.split('-')
-            var dateReorg = `${currentDate[1]}/${currentDate[2].slice(0,2)}/${currentDate[0]}`
-            var currentTemp = `Temp: ${currentData.main.temp}°F`
-            var currentWind = `Wind: ${currentData.wind.speed}MPH`
-            var currentHumidity = `Humidity: ${currentData.main.humidity}%`
-            var forecastArray = [data.list[0], data.list[8],data.list[16],data.list[24],data.list[32]]
-            console.log(data)
-            console.log(cityName)
-            console.log(dateReorg)
-            console.log(currentTemp)
-            console.log(currentWind)
-            console.log(currentHumidity)
+            var forecastArray = [data.list[0], data.list[8], data.list[16], data.list[24], data.list[32]]
             console.log(forecastArray)
+            
+            grabCurrentdata(cityName, currentData, inputCityEl)
+
         })
-
-
-    // add to HTML main
-    var cityHeaderEl = document.querySelector('#cityheader')
-    cityHeaderEl.textContent = inputCityEl.toUpperCase()
-    // add to HTML aside
-    var prevCitiesEl = document.querySelector('#previouscities')
-    prevCitiesEl.innerHTML += `<a href="#" class="list-group-item list-group-item-action active" aria-current="true">${inputCityEl.toUpperCase()}</a>`
-
 }
 
 submitBtn.addEventListener('click', handleSubmittedCity)
