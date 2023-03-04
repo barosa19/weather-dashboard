795604378522cf05246de2b29284bf9ac50a8324
Brosa <brunoarosa1997@gmail.com>
2023-03-04 15:23:13 -0500

    added a function that checks api for user input

--- assets/script.js
+++ assets/script.js
@@ -15,29 +15,24 @@ function grabCurrentdata(name, obj, txt) {
     console.log(currentWind)
     console.log(currentHumidity)
 
-        // add to HTML main
-        var cityHeaderEl = document.querySelector('#cityheader')
-        cityHeaderEl.innerHTML = `${txt} (${dateReorg}) <img src=${iconSrc}>`
+    // add to HTML main
+    var cityHeaderEl = document.querySelector('#cityheader')
+    cityHeaderEl.innerHTML = `${txt} (${dateReorg}) <img src=${iconSrc}>`
 
-        // add to HTML aside
-        //var prevCitiesEl = document.querySelector('#previouscities')
-        //prevCitiesEl.innerHTML += `<a href="#" class="list-group-item list-group-item-action active" aria-current="true">${inputCityEl.toUpperCase()}</a>`
+    // add to HTML aside
+    //var prevCitiesEl = document.querySelector('#previouscities')
+    //prevCitiesEl.innerHTML += `<a href="#" class="list-group-item list-group-item-action active" aria-current="true">${inputCityEl.toUpperCase()}</a>`
 }
+function checkApi(inputVal) {
 
-function handleSubmittedCity(event) {
-    event.preventDefault()
-
-    var inputCityEl = document.querySelector('#inputCity').value.trim()
-
-    if (!inputCityEl) {
-        alert('You need to enter a city!');
-        return;
-    }
-    // API URL
-    var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${inputCityEl.toLowerCase()}&appid=9d4a069564580646340081bd6f2b3b20&units=imperial`
+    var weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${inputVal.toLowerCase()}&appid=9d4a069564580646340081bd6f2b3b20&units=imperial`
 
     fetch(weatherURL)
         .then(function (response) {
+            if (!response.ok) {
+                throw alert('You need to enter a valid city!')
+            }
+
             return response.json()
         })
         .then(function (data) {
@@ -46,10 +41,24 @@ function handleSubmittedCity(event) {
             var currentData = data.list[0]
             var forecastArray = [data.list[0], data.list[8], data.list[16], data.list[24], data.list[32]]
             console.log(forecastArray)
-            
-            grabCurrentdata(cityName, currentData, inputCityEl)
+
+            grabCurrentdata(cityName, currentData, inputVal)
 
         })
 }
+
+function handleSubmittedCity(event) {
+    event.preventDefault()
+
+    var inputCityEl = document.querySelector('#inputCity').value.trim()
+    // Alerts user if search bar is empty
+    if (!inputCityEl) {
+        alert('You need to enter a city!');
+        return;
+    }
+
+    checkApi(inputCityEl)
+}
+
 
 submitBtn.addEventListener('click', handleSubmittedCity)
